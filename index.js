dbb83a032b7739229e65f06f22460dc8ce1c7c0c
hyoilll <50327128+hyoilll@users.noreply.github.com>
2020-09-27 16:40:07 +0900

    Js: navbar

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const navbar = document.querySelector("#navbar");
+const navbar_height = navbar.getBoundingClientRect().height;
+
+// Make navbar transparent when  on the top
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbar_height) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
