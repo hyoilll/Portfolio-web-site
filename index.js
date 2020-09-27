c5754b6f05c04356886dbe2d3d9b0ab8329bfb24
hyoilll <50327128+hyoilll@users.noreply.github.com>
2020-09-27 16:58:58 +0900

    Js: Navbar menu click

--- index.js
+++ index.js
@@ -3,11 +3,31 @@
 const navbar = document.querySelector("#navbar");
 const navbar_height = navbar.getBoundingClientRect().height;
 
-// Make navbar transparent when  on the top
+// 上に上がったらNavbarが透明になる
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbar_height) {
     navbar.classList.add("navbar--dark");
   } else {
     navbar.classList.remove("navbar--dark");
+  }
+});
+
+// メニューを押すとメニューの領域に移動する
+const navbar_menu = document.querySelector(".navbar__menu");
+navbar_menu.addEventListener("click", (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+
+  if (link === null) {
+    return;
+  } else {
+    console.log(e.target.dataset.link);
+  }
+
+  const scrollTo = document.querySelector(link);
+  if (scrollTo === null) {
+    return;
+  } else {
+    scrollTo.scrollIntoView({ behavior: "smooth" });
   }
 });
