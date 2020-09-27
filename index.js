0c6141dd0bdcf9f2af59ed36776a66c24e26dc3f
hyoilll <50327128+hyoilll@users.noreply.github.com>
2020-09-27 20:31:52 +0900

    Js: work btn

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ workBtn.addEventListener("click", (e) => {
     return;
   }
 
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  target.classList.add("selected");
+
   const projectContainer = document.querySelector(".work__projects");
   projectContainer.classList.add("anim-out");
 
